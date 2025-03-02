475e9d91a424045d972ff6fda30dd746cd979738
mynk.chn <mynkchn.pu@gmail.com>
2025-03-02 09:55:32 +0530

    updated

--- inventory/tracker/admin.py
+++ inventory/tracker/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from tracker.models import Contact
+from tracker.models import Contact,Category,Item
+
+class category(admin.ModelAdmin):
+    list_display=['name','description']
+    search_fields=['created_at']
+
+class item(admin.ModelAdmin):
+    list_display=['name','category','quantity','description']
+    search_fields=['name']
 
 admin.site.register(Contact)
+admin.site.register(Category,category)
+admin.site.register(Item,item)
